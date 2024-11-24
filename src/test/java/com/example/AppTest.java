3f5104090fd2fab239492d54fd9fcb4af542e57b
Your Name <you@example.com>
2024-11-24 16:37:43 +0300

    myapp

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -3,9 +3,15 @@ package com.example;
 import org.junit.Assert;
 import org.junit.Test;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Step;
+import io.qameta.allure.Feature;
+
 public class AppTest {
 
     @Test
+    @Description("Проверка работы метода add() с положительными числами")
+    @Feature("Calculator")
     public void testAdd() {
         App app = new App();
         int result = app.add(2, 3);
