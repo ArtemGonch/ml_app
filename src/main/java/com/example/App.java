8d5cf9ceb2a9452b8d5dda80c34cbd20ab05f68b
Your Name <you@example.com>
2024-11-24 19:14:40 +0300

    myapp

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,9 +1,6 @@
 package com.example;
 
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello, Maven!");
-    }
 
     public int add(int a, int b) {
         return b + a;
